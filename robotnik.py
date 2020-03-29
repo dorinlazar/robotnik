35ed5d55ef4252bd0bf48ac564ee4673fe37a0af
Dorin Lazăr <dorin.lazar@gmail.com>
2020-03-29 11:23:38 +0300

    Added a bit of logic

--- robotnik.py
+++ robotnik.py
@@ -1,7 +1,9 @@
+from botfuncs.echobot import Echo
 import os
 import discord
 import yaml
 from roboapi import MessageHandler
+
 
 class MessageHub(object):
   def __init__(self):
@@ -12,11 +14,16 @@ class MessageHub(object):
     assert sc not in self.__handlers
     self.__handlers[sc] = t
 
+
 class RoboClient(discord.Client):
   def __init__(self):
     discord.Client.__init__(self)
-    self.__hub = MessageHub()
-    
+    self.__handlers = {}
+
+  def register(self, t: MessageHandler):
+    sc = t.shortcode()
+    assert sc not in self.__handlers
+    self.__handlers[sc] = t
 
   async def on_ready(self):
     print('Logged on as {0}!'.format(self.user))
@@ -24,13 +31,21 @@ class RoboClient(discord.Client):
   async def on_message(self, message):
     if message.author == client.user:
       return
+    txt = message.content
+    if txt.startswith('!'):
+      pos = txt.find(' ')
+      moniker = txt[1:] if pos == -1 else txt[1:pos]
+      payload = '' if pos == -1 else txt[pos+1:].strip()
+      handler = self.__handlers.get(moniker, None)
+      if handler:
+        await message.channel.send(handler.on_message(payload))
+      print('Request from {0.author}: {0.content}'.format(message))
 
-    if message.content.startswith('$hello'):
-      await message.channel.send('Hello!')
-    print('Message from {0.author}: {0.content}'.format(message))
 
 client = RoboClient()
 
+# proceed in a dumb way
+client.register(Echo())
 
 with open(os.path.expanduser('~/.robotnik.yml'), 'r') as ymlfile:
   cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
