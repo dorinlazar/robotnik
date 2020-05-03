368623dec3178e86268553c4cfb3108fb758709c
Dorin Lazăr <dorin.lazar@gmail.com>
2020-05-03 22:45:54 +0300

    Support multiple users to be monitored

--- botfuncs/twitterbot.py
+++ botfuncs/twitterbot.py
@@ -4,25 +4,22 @@ import threading
 
 
 class TwitterBot(object):
-  def __init__(self, ck=None, cks=None, at=None, ats=None, throttle=60):
+  def __init__(self, ck=None, cks=None, at=None, ats=None, throttle=60, users=[]):
     self.__api = twitter.Api(consumer_key=ck, consumer_secret=cks, access_token_key=at, access_token_secret=ats)
-    self.__since = None
+    self.__since = {}
     self.__throttle = throttle
     self.__counter = 0
+    self.__users = users
 
-  def on_timer(self):
-    self.__counter = self.__counter - 1
-    if self.__counter > 0:
-      return []
-    self.__counter = self.__throttle
-    perform_send = self.__since is not None
-    kwargs = {'count': 20, 'screen_name': 'dorinlazar', 'exclude_replies': True}
+  def fetch_last_tweets(self, user):
+    perform_send = user in self.__since
+    kwargs = {'count': 20, 'screen_name': user, 'exclude_replies': True}
     if perform_send:
-      kwargs['since_id'] = self.__since
+      kwargs['since_id'] = self.__since[user]
     timeline = self.__api.GetUserTimeline(**kwargs)
     res = []
     if timeline:
-      self.__since = timeline[0].id
+      self.__since[user] = timeline[0].id
       if perform_send:
         for msg in reversed(timeline):
           print('Received tweet: ', msg.text)
@@ -30,4 +27,14 @@ class TwitterBot(object):
                                                  'https://twitter.com/i/web/status/{}'.format(msg.id) if msg.truncated or len(msg.urls) == 0 else '')))
       else:
         print('Last tweet:', timeline[0].id, timeline[0].text)
+    return res
+
+  def on_timer(self):
+    self.__counter = self.__counter - 1
+    if self.__counter > 0:
+      return []
+    self.__counter = self.__throttle
+    res = []
+    for u in self.__users:
+      res.extend(self.fetch_last_tweets(u))
     return res
